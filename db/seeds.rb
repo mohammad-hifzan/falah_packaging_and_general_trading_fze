7a36c9fb46f2d8a97aabb29c985d59c7c8d7b658
Mohammad Hifzan <mohammad.hifzan@octalogic.in>
2026-07-09 20:46:42 +0530

    fix: migration run miss

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-AdminUser.create!(email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PASSWORD'], password_confirmation: ENV['ADMIN_PASSWORD']) if Rails.env.production?
+AdminUser.create!(email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PASSWORD'], password_confirmation: ENV['ADMIN_PASSWORD']) if Rails.env.production? && !AdminUser.exists?(email: ENV['ADMIN_EMAIL'])
